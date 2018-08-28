f40ad9243cd86b9181435e8c79933541d9e5eb13
Jeroen Deviaene <jeroen@deviaene.eu>
2018-08-28 14:49:14 +0200

    Add keywords translations

---
+++ src/en/keywords.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ *
+ *  These are translated keywords used for alt tags for images
+ *
+ */
+
+return [
+
+    'animals' => 'animals',
+    'board' => 'board',
+    'boardgames' => 'board games',
+    'box' => 'box',
+    'boxback' => 'back of the box',
+    'card' => 'card',
+    'cards' => 'cards',
+    'characters' => 'characters',
+    'coins' => 'coins',
+    'collection' => 'collection',
+    'cubes' => 'cubes',
+    'components' => 'components',
+    'dice' => 'dice',
+    'dragon' => 'dragon',
+    'dragons' => 'dragons',
+    'gameboard' => 'game board',
+    'gameplay' => 'gameplay',
+    'gems' => 'gems',
+    'horses' => 'horses',
+    'locations' => 'locations',
+    'logo' => 'logo',
+    'manual' => 'manual',
+    'miniature' => 'miniature',
+    'miniatures' => 'miniatures',
+    'money' => 'money',
+    'pawns' => 'pawns',
+    'planet' => 'planet',
+    'planets' => 'planets',
+    'tiles' => 'tiles',
+
+];
---
+++ src/nl/keywords.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ *
+ *  Dit zijn sleutelwoorden die gebruikt worden voor de alt attributen van afbeeldingen
+ *
+ */
+
+return [
+
+    'animals' => 'dieren',
+    'board' => 'bord',
+    'boardgames' => 'bordspellen',
+    'box' => 'doos',
+    'boxback' => 'achterkant van de doos',
+    'card' => 'kaart',
+    'cards' => 'kaarten',
+    'characters' => 'personages',
+    'coins' => 'munten',
+    'collection' => 'collectie',
+    'cubes' => 'kubussen',
+    'components' => 'componenten',
+    'dice' => 'dobbelstenen',
+    'dragon' => 'draak',
+    'dragons' => 'draken',
+    'gameboard' => 'spelbord',
+    'gameplay' => 'speelwijze',
+    'gems' => 'edelstenen',
+    'horses' => 'paarden',
+    'locations' => 'locaties',
+    'logo' => 'logo',
+    'manual' => 'handleiding',
+    'miniature' => 'miniatuur',
+    'miniatures' => 'miniaturen',
+    'money' => 'geld',
+    'pawns' => 'pionnen',
+    'planet' => 'planeet',
+    'planets' => 'planeten',
+    'tiles' => 'tegels',
+
+];
